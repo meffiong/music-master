49e9db4fb0857fddcf15f5c6dbf199318e6ff198
Mandela Effah Effiong <FBN@Mandelas-MacBook-Pro.local>
2019-08-27 09:42:29 -0500

    Add build folder

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_ADDRESS = 'https://spotify-api-wrapper.appspot.com/';
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      artistSearch: '',
+      artist: null,
+      tracks: null
+    }
+  }
+
+  handleChange = (e) => {
+    console.log('event', e.target.value);
+    this.setState({artistSearch: e.target.value})
+  }
+
+  searchArtist = () => {
+    console.log('this.state', this.state)
+
+    fetch(`${API_ADDRESS}/artist/${this.state.artistSearch}`)
+      .then( (response) => response.json() )
+      .then( (json) => console.log('json', json) );
+
+      if(json.artist.total > 0){
+        const artist = json.artist.items[0]
+        console.log('artist', artist)
+        this.setState({artist});
+      }
+
+      fetch();
+  }
+
+  handeleKeyPress = e => {
+    if(e.key === 'Enter'){
+      this.searchArtist();
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Music Master</h1>
+        <input type="text" onChange={this.handleChange} onKeyPress={this.handeleKeyPress} />
+        <button onClick={this.searchArtist}>Search</button>
+      </div>
+    );
+  }
 }
 
 export default App;
